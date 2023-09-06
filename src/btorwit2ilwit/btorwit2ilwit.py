6d4243362f8ba555426253a0289443ad5ff75d02
cgjohannsen <chrisjohannsen8@gmail.com>
2023-09-06 14:01:41 -0500

    intermediate progress on translator

--- src/btorwit2ilwit/btorwit2ilwit.py
+++ src/btorwit2ilwit/btorwit2ilwit.py
@@ -2,5 +2,11 @@ from btor_witness import *
 from il_witness import *
 
 
-def translate(btor_witness: BtorWitness):
+def translate(btor_witnesses: set[BtorWitness]) -> Optional[ILCheckSystemResponse]:
+
+
+    return None
+
+
+if __name__ == "__main__":
     pass
